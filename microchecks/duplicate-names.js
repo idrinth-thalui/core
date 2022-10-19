37eb8ec11b723fa5bd0458adf4d76bb93f2c3860
Björn Büttner <Idrinth@users.noreply.github.com>
2022-10-19 13:20:44 +0200

    Create duplicate-names.js

---
+++ microchecks/duplicate-names.js
@@ -0,0 +1,26 @@
+import {readdirSync, lstatSync} from 'fs';
+
+const filenames = {};
+
+function readDir(directory)
+{
+    for (const file of readdirSync(directory, 'utf8')) {
+        if (file.match(/\.md$/)) {
+            filenames[file.toLowerCase()] = filenames[file.toLowerCase()] || 0;
+            filenames[file.toLowerCase()]++;
+        } else if (lstatSync(`${directory}/${file}`).isDirectory()) {
+            readDir(`${directory}/${file}`);
+        }
+    }
+}
+
+readDir(process.cwd());
+let error = false;
+for (const file of Object.keys(filenames)) {
+    if (filenames[file] > 1) {
+        console.error(`${file} is used ${filenames[files]}x times.`);
+        error = true;
+    }
+}
+
+process.exit(error?1:0);
