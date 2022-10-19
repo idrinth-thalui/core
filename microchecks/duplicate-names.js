58113116a272cfa93a3a10dfaac3aec1755e326e
Björn Büttner <Idrinth@users.noreply.github.com>
2022-10-19 13:48:20 +0200

    Update duplicate-names.js

--- microchecks/duplicate-names.js
+++ microchecks/duplicate-names.js
@@ -6,8 +6,9 @@ function readDir(directory)
 {
     for (const file of readdirSync(directory, 'utf8')) {
         if (file.match(/\.md$/i)) {
-            filenames[file.toLowerCase()] = filenames[file.toLowerCase()] || 0;
-            filenames[file.toLowerCase()]++;
+            filenames[file.toLowerCase()] = filenames[file.toLowerCase()] || {files:[],count:0};
+            filenames[file.toLowerCase()].count++;
+            filenames[file.toLowerCase()].files.push(`${directory}/${file}`.toLowerCase());
         } else if (file!=="node_modules" && lstatSync(`${directory}/${file}`).isDirectory()) {
             readDir(`${directory}/${file}`);
         }
@@ -17,8 +18,11 @@ function readDir(directory)
 readDir(process.cwd());
 let error = false;
 for (const file of Object.keys(filenames)) {
-    if (filenames[file] > 1 && file!=="readme.md") {
-        console.error(`${file} is used ${filenames[file]}x times.`);
+    if (filenames[file].count > 1 && file!=="readme.md") {
+        console.error(`${file} is used ${filenames[file].count}x times.`);
+        for (const path of filenames[file].files) {
+            console.log(`  ${path}`);
+        }
         error = true;
     }
 }
